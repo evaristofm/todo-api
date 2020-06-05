49ef1de49aea809157142ab3c875161bb96fb42b
Evaristo <evaristofm@gmail.com>
2020-06-05 18:08:19 -0300

    Melhorando a interface do servico web

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask import jsonify, abort, make_response, request
+from flask import Flask, jsonify, abort, make_response, request, url_for
+
 
 
 app = Flask(__name__)
@@ -20,10 +20,20 @@ tasks = [
     }
 ]
 
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
+
 
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
 
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>')
